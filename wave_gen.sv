eb70ceb7d9f83d7d5009e7c0388dcbb8e632bdb8
Nguyễn Đăng Khánh <khanh.nguyendang@hcmut.edu.vn>
2025-02-26 12:39:33 +0700

    Update wave_gen.sv

--- wave_gen.sv
+++ wave_gen.sv
@@ -2,13 +2,13 @@ module wave_gen(
     input logic clk,
     input logic rst,
     input logic signed [7:0] wave_amp,
-	 input logic signed [7:0] noise_amp, 
+    input logic signed [7:0] noise_amp, 
     input logic [7:0] freq,
     input logic [1:0] wave_sel,
     input logic inj_sel,
-	 input logic [7:0] duty_cycle,
+    input logic [7:0] duty_cycle,
     output logic signed [15:0] wave_out, 
-	 output logic signed [15:0] noise_out
+    output logic signed [15:0] noise_out
 );
 
 logic signed [7:0] wave_preout;
