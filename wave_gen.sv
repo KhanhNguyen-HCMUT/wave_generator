b0d1c0bb7eaea0fe130decc69d7d81f70b6387e0
Nguyễn Đăng Khánh <khanh.nguyendang@hcmut.edu.vn>
2025-03-06 12:17:40 +0700

    Update wave_gen.sv

--- wave_gen.sv
+++ wave_gen.sv
@@ -4,7 +4,7 @@ module wave_gen(
     input logic signed [7:0] wave_amp,
     input logic signed [7:0] noise_amp, 
     input logic [7:0] freq,
-    input logic [1:0] wave_sel,
+    input logic [2:0] wave_sel,
     input logic inj_sel,
     input logic [7:0] duty_cycle,
     output logic signed [15:0] wave_out, 
@@ -13,6 +13,7 @@ module wave_gen(
 
 logic signed [7:0] wave_preout;
 logic signed [7:0] sine_wave;
+logic signed [7:0] ecg_wave;
 logic signed [7:0] rec_wave;
 logic signed [7:0] saw_wave;
 logic signed [7:0] tria_wave;
@@ -25,14 +26,17 @@ sine_gen sine_gen(.sine_out(sine_wave), .rst(rst), .phase(phase_out));
 rec_gen rec_gen(.rec_out(rec_wave), .rst(rst), .phase(phase_out), .duty_cycle(duty_cycle));
 saw_gen saw_gen(.saw_out(saw_wave), .rst(rst), .phase(phase_out));
 tria_gen tria_gen(.tria_out(tria_wave), .rst(rst), .phase(phase_out));
+ecg_gen ecg_gen(.ecg_out(ecg_wave), .rst(rst), .phase(phase_out));
 noise_gen noise_gen(.lfsr(noise), .rst(rst), .clk(phase_out));
 // Chọn dạng sóng
 always @(*) begin
     case (wave_sel) 
-        2'b00: wave_preout = sine_wave;
-        2'b01: wave_preout = rec_wave;
-        2'b10: wave_preout = saw_wave;
-        2'b11: wave_preout = tria_wave;
+        3'b000: wave_preout = sine_wave;
+        3'b001: wave_preout = rec_wave;
+        3'b010: wave_preout = saw_wave;
+        3'b011: wave_preout = tria_wave;
+		  3'b100: wave_preout = ecg_wave;
+		  default: wave_preout = 0;
     endcase
 end
 
@@ -44,7 +48,7 @@ assign noise_out = noise * noise_amp; //Đưa nhiễu ra ngoài để quan sát
 always @(*) begin
     case (inj_sel) 
         1'b0: wave_out = mult_result;  
-        1'b1: wave_out = mult_result + noise; 
+        1'b1: wave_out = mult_result + noise_out; 
     endcase
 end
 
